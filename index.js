f37a5c7f66a8088c826b8a1056a0873412df338b
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-15 16:26:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = { foo: 'bar' }
+
+function updateObjectWithKeyAndValue()
+{
+   var obj = { prop: '1' }
+   var newObj = Object.assign({}, obj)
+   newObj
+   delete newObj.foo 
+   newObj 
+   obj 
+}
