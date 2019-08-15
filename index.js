80f1366317c6ca88833e97d04d5b827f0c8f690c
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-15 16:28:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function updateObjectWithKeyAndValue()
    newObj 
    obj 
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+ 
+const recipe = { eggs: 3 }
+ 
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
+// returns { eggs: 3, flour: '3 cups' }
